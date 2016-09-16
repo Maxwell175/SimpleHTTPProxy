e948acef102da3f57b9aa8ee7af0858029bfa3ec
MDTech-us-MAN <admin@mdtech.us>
2016-09-16 13:37:54 -0400

    Update httpthread.cpp

--- httpthread.cpp
+++ httpthread.cpp
@@ -11,13 +11,6 @@ HTTPThread::HTTPThread(int socketDescriptor, QObject* parent)
     qDebug() << "HTTPThread()";
 }
 
-// When the thread pool kicks up
-// it's going to hit this run, and it's going to do this time consuming task.
-// After it's done, we're going to send the results back to our main thread.
-// This runs in the separate thread, and we do not have any control over this thread,
-// but Qt does.
-// This may just stay in the queue for several ms depending on how busy the server is.
-
 void HTTPThread::run()
 {
     qDebug() << "HTTPThread::run()";
